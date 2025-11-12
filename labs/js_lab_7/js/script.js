0b56dfd9e7ba3b8fc7924b0e18e3a6992d88a822
Brandon <brandonmei888@gmail.com>
2025-11-12 11:51:34 -0600

    Done

---
+++ labs/js_lab_7/js/script.js
@@ -0,0 +1,22 @@
+function init() {
+
+
+    const button = document.getElementById("entrybutton");
+    const input = document.getElementById("entryinput");
+    const output = document.getElementById("textoutput");
+
+    button.addEventListener("click", function() {
+        const message = input.value;
+        if (message === "") 
+        {
+            alert("Please enter a message");
+        } 
+        else
+        {
+            alert("Brandon Mei: " + message);
+            output.textContent = message;
+        }
+    });
+}
+
+window.addEventListener('load', init);

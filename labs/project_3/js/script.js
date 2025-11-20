9495d09284e6195226df6c7d9e1fc39631792c08
Brandon <brandonmei888@gmail.com>
2025-11-20 11:21:00 -0600

    initial

--- labs/project_3/js/script.js
+++ labs/project_3/js/script.js
@@ -1,6 +1,24 @@
 function init() {
 alert("Google Maps API loaded successfully!");
-    const map = new google.maps.Map(document.getElementById("map"), {
+var ele = document.getElementById('canvas');
+var myLocation = new google.maps.LatLng(37.7749, -122.4194); // Example coordinates (San Francisco)
+var mapOptions = {
+  center: myLocation,
+  zoom: 12,
+  mapTypeId: google.maps.MapTypeId.SATELLITE,
+  mapTypeControlOptions: {
+    style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+    position: google.maps.ControlPosition.TOP_RIGHT
+},
+var myMap = new google.maps.Map(ele, mapOptions),
+
+var marker = new google.maps.Marker({
+  position: myLocation,
+  map: myMap,
+  title: "My Location"
+}),
+
+}
 }
 
-window.addEventListener('load', init);
+google.maps.event.addDomListener(window,'load', init);
